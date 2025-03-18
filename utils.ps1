87e83c5c30a295be8f15d50b7bf24d1a81dc84b1
Gabgodd <67527493+Gabgodd@users.noreply.github.com>
2025-03-18 12:21:16 -0300

    Update utils.ps1

--- utils.ps1
+++ utils.ps1
@@ -19,3 +19,28 @@ function Write-Log {
     Add-Content -Path $LogFile -Value $logMessage
     Write-Output $logMessage
 }
+
+# Function to download files using BITS
+function Download-FileWithBITS {
+    param (
+        [string]$url,
+        [string]$output
+    )
+
+    Write-Log "Starting BITS transfer for $url..."
+    $bitsJob = Start-BitsTransfer -Source $url -Destination $output -Asynchronous
+
+    # Wait for the BITS transfer to complete
+    while ($bitsJob.JobState -eq [Microsoft.BackgroundIntelligentTransfer.Management.JobState]::Transferring) {
+        Write-Host "Downloading... $($bitsJob.BytesTransferred) of $($bitsJob.BytesTotal) bytes transferred."
+        Start-Sleep -Seconds 5
+    }
+
+    if ($bitsJob.JobState -eq [Microsoft.BackgroundIntelligentTransfer.Management.JobState]::Transferred) {
+        Complete-BitsTransfer -BitsJob $bitsJob
+        Write-Log "Download completed successfully for $url."
+    } else {
+        Remove-BitsTransfer -BitsJob $bitsJob -Force
+        Throw "BITS transfer failed with state: $($bitsJob.JobState)"
+    }
+}
